822de5280fe49e83173db56dfbb858bd25477777
Taylor McKay <taylormckay68@gmail.com>
2017-08-27 09:20:29 -0600

    through step 3

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import store from './store';
+import {BrowserRouter} from 'react-router-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<BrowserRouter>
+    <Provider store={store}>
+        <App />
+    </Provider>
+</BrowserRouter>, 
+document.getElementById('root'));
 registerServiceWorker();
 
